36f4ff487f6f6be4e7154df63628abe11a4ac113
Federico Zimmerman <fzimmerman@Federicos-MacBook-Pro.local>
2017-01-10 18:37:17 -0300

    spanish strings created

--- app/build/generated/source/r/debug/tedxperiments/math/entrenamente/R.java
+++ app/build/generated/source/r/debug/tedxperiments/math/entrenamente/R.java
@@ -11,8 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int acierto=0x7f020000;
@@ -270,11 +270,23 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060000;
-        public static final int app_name=0x7f060001;
-        public static final int hello_world=0x7f060002;
-        public static final int help_info=0x7f060003;
-        public static final int menu_settings=0x7f060004;
+        public static final int action_settings=0x7f040001;
+        public static final int agreement_message=0x7f040002;
+        public static final int app_name=0x7f040000;
+        public static final int birthdate=0x7f040003;
+        public static final int complete_instruction=0x7f040004;
+        public static final int developer_signature=0x7f040005;
+        public static final int female=0x7f040006;
+        public static final int gender=0x7f040007;
+        public static final int hello_world=0x7f040008;
+        public static final int help_info=0x7f040009;
+        public static final int male=0x7f04000a;
+        public static final int menu_settings=0x7f04000b;
+        public static final int not_reported=0x7f04000c;
+        public static final int play_button=0x7f04000d;
+        public static final int practice_button=0x7f04000e;
+        public static final int stats_button=0x7f04000f;
+        public static final int tutorial_button=0x7f040010;
     }
     public static final class style {
         /**  API 11 theme customizations can go here. 
@@ -285,9 +297,9 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
