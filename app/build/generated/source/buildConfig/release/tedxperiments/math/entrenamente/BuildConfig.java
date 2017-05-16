414b928a49c12d93c3b929da24d41da80f905ad5
Federico Zimmerman <fzimmerman@Federicos-MBP.home>
2017-05-16 20:01:07 +0100

    personal data reordered and version updated

--- app/build/generated/source/buildConfig/release/tedxperiments/math/entrenamente/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/tedxperiments/math/entrenamente/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "tedxperiments.math.entrenamente";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 16;
-  public static final String VERSION_NAME = "2.13";
+  public static final int VERSION_CODE = 17;
+  public static final String VERSION_NAME = "2.14";
   // Fields from build type: release
   public static final String SERVER_URL = "https://dennett-stage.herokuapp.com/api/v1/trials";
 }
