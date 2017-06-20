939807abee7e23bb1654f1be2cd081b055a88804
Federico Zimmerman <fzimmerman@Federicos-MacBook-Pro.local>
2017-06-20 15:33:21 +0100

    project details

--- app/build/generated/source/buildConfig/debug/tedxperiments/math/entrenamente/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/tedxperiments/math/entrenamente/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "tedxperiments.math.entrenamente";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 19;
-  public static final String VERSION_NAME = "1.03";
+  public static final int VERSION_CODE = 20;
+  public static final String VERSION_NAME = "1.04";
   // Fields from build type: debug
   public static final String SERVER_URL = "https://dennett-stage.herokuapp.com/api/v1/trials";
 }
